0016ab97f3e0fd29afd3fc52c6a354e15bf288fe
unknown <sabzevari98.itsu@yahoo.com>
2023-05-08 19:44:55 +0430

    login

--- src/Components/Routes/index.js
+++ src/Components/Routes/index.js
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Category from "../../Pages/Category";
-import Home from "../../Pages/Home";
+import LoginPage from "../../Pages/Login/Login";
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Category />}></Route>
+      <Route path="/login" element={<LoginPage />}></Route>
       <Route path="/:categoryId" element={<Category />}></Route>
     </Routes>
   );
